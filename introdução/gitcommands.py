fa59d87a1c11c41e883fa6052e3d335cb614c6ae
Alan <alanjvpn@gmail.com>
2026-02-04 16:56:14 -0300

    Altualizando arquivos nas pastas e Inicio Laboratorio "Biblioteca"

---
+++ introdução/gitcommands.py
@@ -0,0 +1,26 @@
+# git init - Inicializa um novo repositório Git em um diretório.
+# git clone [url] - Clona um repositório remoto para o seu computador local.
+# git status - Exibe o estado atual do repositório, mostrando arquivos modificados, não rastreados e prontos para commit.
+# git add [arquivo] - Adiciona arquivos específicos à área de stage para o próximo commit.
+# git add . - Adiciona todos os arquivos modificados e novos à área de stage.
+# git commit -m "mensagem" - Cria um commit com as alterações na área de stage, incluindo uma mensagem descritiva.
+
+# git push - Envia os commits locais para o repositório remoto.
+# git pull - Puxa as últimas alterações do repositório remoto para o seu repositório local.
+# git branch - Lista todas as branches no repositório atual.
+# git branch [nome-da-branch] - Cria uma nova branch com o nome especificado.
+# git checkout [nome-da-branch] - Muda para a branch especificada.
+# git merge [nome-da-branch] - Mescla a branch especificada na branch atual.
+
+# git log - Exibe o histórico de commits do repositório.
+# git diff - Mostra as diferenças entre arquivos modificados e a última versão commitada.
+# git remote -v - Lista os repositórios remotos associados ao repositório local.
+
+# git fetch - Busca as últimas alterações do repositório remoto sem mesclá-las automaticamente.
+# git reset [arquivo] - Remove um arquivo específico da área de stage.
+# git reset --hard - Restaura o repositório para o último commit, descartando todas as alterações locais.
+
+# git stash - Salva temporariamente as alterações não commitadas para que você possa trabalhar em outra coisa.
+# git stash pop - Restaura as alterações salvas pelo comando git stash.
+# git tag [nome-da-tag] - Cria uma tag para marcar um ponto específico na história do commit.
+# git show [tag ou commit] - Exibe informações detalhadas sobre uma tag ou commit específico.
